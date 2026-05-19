954c7f64250ffb2034c0c60e0c88b17e94d92300
REEN <your-email@example.com>
2026-05-19 09:30:53 -0700

    Add admin page

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public')); // serves files from /public folder
 
 // MongoDB connection
 const MONGO_URI = process.env.MONGO_URI;
